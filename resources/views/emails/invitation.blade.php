bddb541fdf306a6a60b1f287194654aef7b90645
Wagih <30524923+AndrewWagih@users.noreply.github.com>
2023-12-13 10:45:27 +0200

    Update invitation.blade.php

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -7,7 +7,116 @@
     <title>
         invitation
     </title>
+    <style>
+        .my-row {
+            margin-right: auto;
+            margin-left: 115px;
+        }
+
+        .my-row img:nth-child(1) {
+
+            height: 55px;
+            margin-right: 20px;
+
+        }
+
+        .my-row img:nth-child(2) {
+
+            height: 75px;
+            margin-right: 20px;
+
+        }
+
+        .my-row img:nth-child(3) {
+
+            height: 53px;
+        }
+
+        .rectangle {
+            position: absolute;
+            height: 100%;
+            width: 102px;
+            left: 0;
+            top: 0;
+        }
+
+
+        @media (min-width: 992px) {
+
+
+
+
+
+
+            #component3 {
+
+                margin: 0;
+                width: 65%;
+                margin-left: auto;
+
+            }
+
+        }
+
+        @media (max-width: 991px) {
+
+
+
+
+
+
+
+            #component3 {
+                width: 100%;
+            }
+
+            .my-p {
+
+                height: 55px !important;
+                overflow: hidden;
+
+            }
+        }
+
+
+        @media (max-width: 600px) {
+
+
+            .my-row {
+                margin-right: auto;
+                margin-left: 75px;
+            }
+
+            .my-row img:nth-child(1) {
+
+                height: 38px;
+                margin-right: 20px;
+
+            }
+
+            .my-row img:nth-child(2) {
+
+                height: 51px;
+                margin-right: 20px;
+
+            }
+
+            .my-row img:nth-child(3) {
+
+                height: 33px;
+            }
+
+            .rectangle {
+                position: absolute;
+                height: 100%;
+                width: 75px;
+                left: 0;
+                top: 0;
+            }
+        }
+    </style>
 </head>
+
 <body dir="rtl">
     <div id="component" class="container-fluid py-3">
         <p class="text-end  m-0 px-3 py-2">
@@ -104,11 +213,11 @@
     </div>
     <div id="component" class="container-fluid w-100 position-relative py-3" style="background-color: #0A2C36;">
         <div class="my-row" style="margin-right: auto;margin-left: 75px;">
-            <img src="{{ asset('assets/images/logo1.png') }}" alt="" style="height: 38px;margin-right: 20px;">
-            <img src="{{ asset('assets/images/logo2.png') }}" alt="" style="height: 51px;margin-right: 20px;">
-            <img src="{{ asset('assets/images/logo3.png') }}" alt="" style="height: 33px;">
+            <img src="{{ asset('assets/images/logo1.png') }}" alt="">
+            <img src="{{ asset('assets/images/logo2.png') }}" alt="">
+            <img src="{{ asset('assets/images/logo3.png') }}" alt="">
         </div>
-        <img class="rectangle" src="{{ asset('assets/images/Rectangle.png') }}" alt="" style="position: absolute;height: 100%;width: 75px;left: 0;top: 0;">
+        <img class="rectangle" src="{{ asset('assets/images/Rectangle.png') }}" alt="" >
     </div>
 </body>
 
