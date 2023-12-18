6ff3f68f5333f2b9fcf53b674322aa825bc56006
Wagih <30524923+AndrewWagih@users.noreply.github.com>
2023-12-18 10:34:49 +0200

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -38,7 +38,7 @@
         }
 
         label {
-            text-align: left;
+            /* text-align: left; */
             margin-bottom: 5px;
         }
 
@@ -160,15 +160,15 @@
     
     </style>
 </head>
-<body>
+<body dir="rtl">
     <div class="container">
         <div class="card">
-          <h2>Admin Login</h2>
+          <h2>تسجيل دخول</h2>
         @if (\Session::has('error-message'))
         <div class="alert alert--error">
             <i class="fa fa-times-circle fa-2xl icon"></i> 
             <div class="content">
-                <div class="title">ERROR</div>
+                <div class="title">خطاء</div>
                 <div class="body">
                     {!! \Session::get('error-message') !!}
                 </div>
@@ -180,7 +180,7 @@
         <div class="alert alert--error">
             <i class="fa fa-times-circle fa-2xl icon"></i> 
             <div class="content">
-                <div class="title">ERROR</div>
+                <div class="title">خطاء</div>
                 <div class="body">
                     {!! implode('', $errors->all('<div>:message</div>')) !!}
                 </div>
@@ -191,25 +191,25 @@
 
           <form method="POST" action="{{route('login.request')}}">
             @csrf
-            <label for="email">email</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email">
+            <label for="email">الايميل</label>
+            <input type="email" id="email" name="email" placeholder="ادخل الايميل">
             <div style="
             position: relative;
             display: block;
             width: 93%;
         ">
-                <label for="password" style="display: block;width:100%">Password</label>
-                <input type="password" id="password" class="password" name="password" placeholder="Enter your password" style="display: block;width:100%">
+                <label for="password" style="display: block;width:100%">كلمة المرور</label>
+                <input type="password" id="password" class="password" name="password" placeholder="ادخل كلمة المرور" style="display: block;width:100%">
                 <span toggle=".password" class="fa fa-fw fa-eye field-icon toggle-password showPassword" style="    float: left;
                 margin-top: -36px;
                 position: absolute;
-                right: 0;
+                left: 0;
                 z-index: 2;">
                     <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z"/></svg>
                 </span>
             </div>
 
-            <button type="submit">Login</button>
+            <button type="submit">تسجيل الدخزل</button>
           </form>
         </div>
 
